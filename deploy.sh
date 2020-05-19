14de28a1194e2469f612907914f5075a82d3997c
Adam Kariv <adam.kariv@gmail.com>
2020-05-19 18:27:11 +0300

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,12 +4,12 @@ git checkout master && \
 (git branch -D dist || true) && \
 git checkout -b dist && \
 rm .gitignore && \
-ng build --base-href /avid-covider-predictor/ && \
+ng build --prod --base-href /avid-covider-predictor/ && \
 cp dist/avid-covider-predictor/index.html dist/avid-covider-predictor/404.html && \
 git add dist/avid-covider-predictor && \
 git commit -m dist && \
 (git branch -D gh-pages || true) && \
-git subtree split --prefix dist/ui -b gh-pages && \
+git subtree split --prefix dist/avid-covider-predictor -b gh-pages && \
 git push -f publish gh-pages:gh-pages && \
 git checkout master && \
 git branch -D gh-pages && \
